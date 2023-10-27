39ff0a5532c9d93268a32b8b9a4d9da4b1f1f278
Ruediger zu Dohna <snackbox@sinntr.eu>
2023-10-27 11:21:53 +0200

    parallel test execution & detailed logging

--- src/main/java/com/example/ExampleResource.java
+++ src/main/java/com/example/ExampleResource.java
@@ -6,40 +6,50 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.StructuredTaskScope;
+import java.util.concurrent.atomic.AtomicInteger;
 
 @Path("/")
+@RunOnVirtualThread
 public class ExampleResource {
+    private static final Logger log = LoggerFactory.getLogger(ExampleResource.class);
+    private final AtomicInteger parallel = new AtomicInteger();
 
     @RegisterRestClient(baseUri = "http://localhost:8080")
     interface Api {
         @GET @Path("/part/{n}")
         @Produces(MediaType.TEXT_PLAIN)
-        String part(@PathParam("n") int n);
+        String part(@PathParam("n") int n, @QueryParam("n") Integer nn);
     }
 
     @Inject @RestClient Api api;
 
     @GET @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
-    @RunOnVirtualThread
-    public String structuredConcurrencyExample() throws InterruptedException, ExecutionException {
+    public String hello(@QueryParam("n") Integer n) throws InterruptedException, ExecutionException {
+        log.info("start hello {} [#{}]", n, parallel.incrementAndGet());
         try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
-            var task1 = scope.fork(() -> api.part(1));
-            var task2 = scope.fork(() -> api.part(2));
+            var hello = scope.fork(() -> api.part(1, n));
+            var world = scope.fork(() -> api.part(2, n));
 
             scope.join().throwIfFailed();
 
-            return task1.get() + ", " + task2.get();
+            return hello.get() + ", " + world.get();
+        } finally {
+            log.info("end hello {} [#{}]", n, parallel.getAndDecrement());
         }
     }
 
     @GET @Path("/part/{n}")
     @Produces(MediaType.TEXT_PLAIN)
-    @RunOnVirtualThread
-    public String part(@PathParam("n") int n) {
+    public String part(@PathParam("n") int n, @QueryParam("n") Integer nn) throws InterruptedException {
+        log.info("start part {} for {} [#{}]", n, nn, parallel.incrementAndGet());
+        Thread.sleep(1000);
+        log.info("done sleeping for part {} for {} [#{}]", n, nn, parallel.getAndDecrement());
         return switch (n) {
             case 1 -> "Hello";
             case 2 -> "World";
--- src/test/java/com/example/ExampleResourceTest.java
+++ src/test/java/com/example/ExampleResourceTest.java
@@ -1,39 +1,101 @@
 package com.example;
 
 import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
+import java.time.Instant;
+import java.util.concurrent.StructuredTaskScope;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import static jakarta.ws.rs.core.MediaType.TEXT_PLAIN;
+import static org.assertj.core.api.BDDAssertions.then;
 
 @QuarkusTest
 public class ExampleResourceTest {
+    @RegisterRestClient(baseUri = "http://localhost:8080")
+    interface Api {
+        @GET @Path("/hello")
+        @Produces(TEXT_PLAIN)
+        String hello(@QueryParam("n") Integer n);
+
+        @GET @Path("/part/{n}")
+        @Produces(TEXT_PLAIN)
+        String part(@PathParam("n") int n);
+    }
+
+    @Inject @RestClient Api api;
 
     @Test
     public void testHelloPart() {
-        given()
-                .when().get("/part/1")
-                .then()
-                .statusCode(200)
-                .body(is("Hello"));
+        var hello = api.part(1);
+
+        then(hello).isEqualTo("Hello");
     }
 
     @Test
     public void testWorldPart() {
-        given()
-                .when().get("/part/2")
-                .then()
-                .statusCode(200)
-                .body(is("World"));
+        var world = api.part(2);
+
+        then(world).isEqualTo("World");
     }
 
     @Test
+    // @Timeout(value = 1_999, unit = MILLISECONDS) // must be faster than two seconds
     public void testHelloEndpoint() {
-        given()
-                .when().get("/hello")
-                .then()
-                .statusCode(200)
-                .body(is("Hello, World"));
+        var hello = api.hello(-1);
+
+        then(hello).isEqualTo("Hello, World");
     }
 
+    @Test
+    public void testManyHellos() throws Exception {
+        var n = 100;
+        var counter = new AtomicInteger();
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            var tasks = IntStream.range(0, n)
+                    .mapToObj(i -> scope.fork(() -> api.hello(i)))
+                    .toList();
+
+            scope.joinUntil(Instant.now().plusSeconds(30)).throwIfFailed();
+
+            tasks.stream().map(StructuredTaskScope.Subtask::get).forEach(text -> {
+                counter.incrementAndGet();
+                then(text).isEqualTo("Hello, World");
+            });
+        }
+
+        then(counter.get()).isEqualTo(n);
+    }
+
+    @Test
+    public void testManySleeps() throws Exception {
+        var n = 1_000_000;
+        var counter = new AtomicInteger();
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            var tasks = IntStream.range(0, n)
+                    .mapToObj(i -> scope.fork(() -> afterDelayReturn(i)))
+                    .toList();
+
+            scope.joinUntil(Instant.now().plusSeconds(30)).throwIfFailed();
+
+            tasks.stream().map(StructuredTaskScope.Subtask::get).forEach(ignored -> counter.incrementAndGet());
+        }
+
+        then(counter.get()).isEqualTo(n);
+    }
+
+    private static int afterDelayReturn(int i) {
+        try {
+            Thread.sleep(1000);
+            return i;
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        }
+    }
 }
